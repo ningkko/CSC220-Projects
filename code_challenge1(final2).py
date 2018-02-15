1050bdb4e2c05bba91d7856982a1bfd9944d8721
YiningHua <izaya1997@163.com>
2018-02-14 22:33:51 -0500

    Update code_challenge1(final2).py

--- code_challenge1(final2).py
+++ code_challenge1(final2).py
@@ -1,67 +1,63 @@
 #Fanghui He, Yining Hua, Yvonne Zhu, Qiaqia Ji
-#Coding_challenge 1 for csc220
-#Feb 5, 2018
-"""In this program we are trying to make a redundancy checker 
-that reads all the files in a specified directory 
-and prints a report of the lines that are identical in any pair of files
-"""
-import os
+#Code-challenge 2
+#Given two strings representing snippets of genes (letters ACGT), 
+#identify the shortest string that could contain them both as subsequences.
 
-#Start to run the program
 def main():
-	list_of_report = compare_contents(input('Directory: '))
-	print_the_report(list_of_report)
-	
-#Given the directory path, compare the contents of any python files in that directory
-def compare_contents(path):
-	#If you just want to operate in the current directory
-	#list_of_filename = [filename for filename in os.listdir(os.getcwd()) if filename.endswith('.py')]
-	list_of_filename = [filename for filename in os.listdir(path) if filename.endswith('.py')]
-	list_of_report = []
-	
-	#Loop through the files in this directory and choose two files to compare
-	for filename1 in list_of_filename[0:-1]:
-		count_of_file = list_of_filename.index(filename1) + 1
-		for filename2 in list_of_filename[count_of_file:]:
-			list_of_identical_line = []
-			line_num_f1 = 0
-			count_of_identical_lines = 0
-			
-			#loop through the lines in the two files using their absolute path and compare them
-			for line1 in open(path+"/"+filename1): 
-				line_num_f1 += 1
-				#skip blank comments and lines
-				if line1.strip()!="'''" and line1.strip()!="#": 
-					line_num_f2 = 0
-					for line2 in open(path+"/"+filename2):
-						line_num_f2 += 1
-						#skip blank lines
-						if line2.strip(): 
-							#if the lines are the same, add the lines and line numbers to the list_of_identical_line
-							if line1 == line2:
-								list_of_identical_line.append([line_num_f1,line_num_f2,line1.strip()])
-								count_of_identical_lines += 1
-								
-			#If we found identical lines, add the list_of_identical_line to list_of_report; if not, do nothing
-			if count_of_identical_lines != 0:
-				list_of_report.append([filename1, filename2, count_of_identical_lines, list_of_identical_line])
-				
-			count_of_file += 1
+	gene1 = input('Please enter a string of genes: ')
+	gene2 = input('Please enter another string of genes: ')
+	gene1,gene2 = gene1.upper(),gene2.upper()
+	if(check_valid_gene(gene1)==1 and check_valid_gene(gene2)==1):
+		shortest_gene, min_length = find_shortest_string(gene1,gene2)
+		print("******************************************** \n string 1 – {0} string 2 – {1} \n shortest string that has both as substring \n {2} (length {3})\n********************************************".format(gene1,gene2,[gene for gene in shortest_gene],min_length))
+	else:
+		raise ValueError("The input string of genes is not valid")
 
-	return list_of_report
-	
-#Function to print out the report
-def print_the_report(list_of_report):
-	for inner_list in list_of_report:
-		print('-'*30)
-		print('File 1: '+inner_list[0])
-		print('File 2: '+inner_list[1])
-		print('Number of identical lines: ',inner_list[2])	
-		print('-'*30)
-		#print all the identical lines in the two files
-		for list in inner_list[3]:
-			print('*** ', list[0],list[1],list[2])
-#End main()
+def check_valid_gene(gene):
+	'''check_valid_gene(string)::= returns TRUE if the dtring is composed og only A,C,G,T.
+	'''
+	#case insensitive
+	set = ['A','C','G','T']
+	for c in gene:
+		if c not in set: 
+			return 0;
+	return 1;
+					
+def find_shortest_string(gene1, gene2):
+	'''find_shortest_string(string1,string2)::= outputs the shortest concatenated string of string1 and string2
+	'''
+	result = []
+	#base case
+	min_len = len(gene1) + len(gene2)
+	result.append(gene1+gene2)
+
+	#Case1: if the result begins with gene1
+	for index1 in range(len(gene1)):
+		#Compare the first digit of gene2 to every digit of gene1
+		if gene2[0] == gene1[index1]:
+			#and if that digit and of the following parts are the same 
+			if gene1[index1:] == gene2[:(len(gene1)-index1)]:
+				#concatenate gene1 and gene2 from that digit.
+				combined_string = gene1 + gene2[(len(gene1)-index1):]
+				#if the new string is shorter than the base case,replace it
+				if len(combined_string) < min_len:
+					min_len = len(combined_string)
+					result[0]=combined_string
+				
+	#Case2: if the result begins with gene2
+	#Do the same thing as in Case 1
+	for index2 in range(len(gene2)):
+		if gene1[0] == gene2[index2]:
+			if gene2[index2:] == gene1[:(len(gene2)-index2)]:
+				combined_string = gene2 + gene1[(len(gene2)-index2):]
+				if len(combined_string) < min_len:
+					min_len = len(combined_string)
+					result[0]=combined_string
+				#If BA has the same length as AB, return both AB and BA
+				elif len(combined_string)==min_len:
+					result.append(combined_string)
+	return result,min_len	
+						
+
 if __name__ == '__main__':
 	main()
-	
