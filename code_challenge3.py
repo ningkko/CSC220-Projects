d0fe2fbe5860287bd3e4494ec660d2ae62c45d20
Qia-Qia <36122453+Qia-Qia@users.noreply.github.com>
2018-02-20 00:17:16 -0500

    Update code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,31 +1,34 @@
-#import numpy as np
+# Fanghui He, Yining Hua, Yvonne Zhu, Qiaqia Ji
+# Code-challenge 3
+# Given a dictionary of items and their (integer, positive-valued) item weights
+# identify whether or not there is a subset that could fill a Prime Pantry Box to exactly 100% 
+# report which items are in the subset
 
-
-def primePantry(items, n_items, total):
-	subsets = [[[''] for i in range(total+1)]for j in range(n_items+1)]
-	i=1
-	for item in items:
-		for j in range(total+1):
-			if (j==items[item]):
-				subsets[i][j] = item.split()
-			for index in range(i):
-				if subsets[index][j]!=[''] and (items[item]+j)<=total: 
-					for string in subsets[index][j]:
-						subsets[i][items[item]+j] = string.split() 
-					subsets[i][items[item]+j]+=item.split()
-		i+=1
-	result = []
-	count = 0
-	for i in range(n_items+1):
-		if subsets[i][total] != ['']:
-			result.append(subsets[i][total])
-			count+=1
-	return result 
+def primePantryV2(items, n_items, total):
+    subsets = [[[''] for i in range(total+1)]for j in range(n_items+1)]
+    i=1
+    for item in items:
+        for j in range(total+1):
+            if (j==items[item]):
+                subsets[i][j] = item.split()
+            for index in range(i):
+                if subsets[index][j]!=[''] and (items[item]+j)<=total: 
+                    for string in subsets[index][j]:
+                        subsets[i][items[item]+j] = string.split() 
+                    subsets[i][items[item]+j]+=item.split()
+        i+=1
+    result = []
+    count = 0
+    for i in range(n_items+1):
+        if subsets[i][total] != ['']:
+            result.append(subsets[i][total])
+            count+=1
+    return result 
 
 def main():
-    l= {"a":3,"b":2,"c":5}
+    #l= {"a":3,"b":2, "c":5, "d": 4, "e": 1}
     total = 5
-    print(primePantry(l, 3, total))
+    print(primePantryV2(l, len(l), total))
      
 if __name__ == '__main__':
-	main()
+    main()
