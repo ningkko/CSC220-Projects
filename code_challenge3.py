e06b4289736598245850748d7d7e8716e907bb4f
FanghuiHe <36115513+FanghuiHe@users.noreply.github.com>
2018-02-21 18:34:52 -0500

    Add files via upload

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,34 +1,41 @@
-# Fanghui He, Yining Hua, Yvonne Zhu, Qiaqia Ji
-# Code-challenge 3
-# Given a dictionary of items and their (integer, positive-valued) item weights
-# identify whether or not there is a subset that could fill a Prime Pantry Box to exactly 100% 
-# report which items are in the subset
+#import numpy as np
 
-def primePantryV2(items, n_items, total):
-    subsets = [[[''] for i in range(total+1)]for j in range(n_items+1)]
-    i=1
-    for item in items:
-        for j in range(total+1):
-            if (j==items[item]):
-                subsets[i][j] = item.split()
-            for index in range(i):
-                if subsets[index][j]!=[''] and (items[item]+j)<=total: 
-                    for string in subsets[index][j]:
-                        subsets[i][items[item]+j] = string.split() 
-                    subsets[i][items[item]+j]+=item.split()
-        i+=1
-    result = []
-    count = 0
-    for i in range(n_items+1):
-        if subsets[i][total] != ['']:
-            result.append(subsets[i][total])
-            count+=1
-    return result 
+
+def primePantry(items, n_items, total):
+	#create a table for dynamic programming
+	subsets = [[[] for i in range(total+1)]for j in range(n_items+1)]
+	i=1
+	# loop through each elements
+	for item in items:
+		#print(i) 
+		for j in range(total+1):
+			# fill in the number which is the item number itself
+			if (j==items[item]) and subsets[i][j] == []:
+				subsets[i][j] = item.split()
+			# fill in the number which is the item number itself 
+			elif (j==items[item]) and subsets[i][j] != []:
+				subsets[i][j].append(item.split())
+				#print(subsets[i][j])
+			for index in range(i):
+				if subsets[index][j]!=[] and (items[item]+j)<=total: 
+					#print(subsets[index][j])
+					for list in subsets[index][j]:
+						#print("list: ",list)
+						subsets[i][items[item]+j].append(subsets[index][j]+item.split())#string.split()
+						#print(subsets[i][items[item]+j])
+		i+=1
+	result = []
+	
+	for i in range(n_items+1):
+		if subsets[i][total] != []:
+			result.append(subsets[i][total])
+			
+	return result 
 
 def main():
-    l= {"a":3,"b":2, "c":5, "d": 4, "e": 1}
+    l= {"a":3,"b":2,"c":5,"d":1,"e":2}
     total = 5
-    print(primePantryV2(l, len(l), total))
+    print(primePantry(l, len(l), total))
      
 if __name__ == '__main__':
-    main()
+	main()
